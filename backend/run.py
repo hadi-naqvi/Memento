fd8321d946b6bc14f13f6b54aaa28f0944b5fdb3
hadi-naqvi <hadi.naqvi@mail.utoronto.ca>
2025-03-22 00:51:50 -0400

    Fixed run.py and added api routes folder

--- backend/run.py
+++ backend/run.py
@@ -3,4 +3,4 @@ from app import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(app, host='127.0.0.1', debug=True, port=5000, allow_unsafe_werkzeug=True)
+    app.run(host='127.0.0.1', debug=True, port=5000)
